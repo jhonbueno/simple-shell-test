37f12c61d27faf22168a10c1337f44dfcc03f653
Juan_Camilo_Arce <camiloarceg@gmail.com>
2021-11-30 18:29:24 +0000

    added funtion count_token and new add. also set the protptipes for strings funtions used in the funtions of the file addtokens.c

--- hsh.h
+++ hsh.h
@@ -11,49 +11,14 @@
 #include <sys/wait.h>
 #include <stdbool.h>
 
-/**
- * struct settings_s - settings for all the program
- * @file: file
- * @line: line
- * @stack: stack
- * @queue: false
- */
-
-typedef struct path_s
-{
-    char* path;
-    struct args_s *next;
-} path_t;
-
-/**
- * struct settings_s - settings for all the program
- * @file: file
- * @line: line
- * @stack: stack
- * @queue: false
- */
-typedef struct args_s
-{
-    char* arg;
-    struct args_s *next;
-} args_t;
-
-/**
- * struct settings_s - settings for all the program
- * @file: file
- * @line: line
- * @stack: stack
- * @queue: false
- */
-
 extern char **environ;
-
-args_t *add(args_t **head, char* arg);
-char **transform(args_t **head);
+int _strlen(char *s);
+int _strncmp(char *s1, char *s2, int n);
+char *_strncpy(char *dest, char *src, int n);
+char **add(char *line);
 char *findpath(char *command, int *ret);
 void _free_double_pointer(char **d_pointer);
 char *str_concat(char *s1, char *s2);
-void free_list(args_t **head);
 char *_strdup(char *str);
 
 #define UNUSED(x) (void)(x);
